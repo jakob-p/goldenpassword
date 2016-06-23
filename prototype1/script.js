e745097d833fc2eaba330d709e9f4313a4c6b360
samuel.souque <samuel.souque@outlook.fr>
2016-06-23 16:09:24 +0200

    Initial commit for the first app's prototype

---
+++ prototype1/script.js
@@ -0,0 +1,18 @@
+﻿'use sctrict';
+
+var app = angular.module('goldenPasswordApp', []);
+
+app.controller('')
+
+app.controller('fetchGridData', ['$scope', '$http', function($scope, $http) {
+    $http.get('passwordData.json').then(function(response) {
+        $scope.gridData = response.data;
+    });
+}]);
+
+app.directive('gpWebsiteGrid', function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'gp-website-grid.html'
+    };
+});
