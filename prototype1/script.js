756792d976942ecdbb74c42c496dd14954536fa0
Samuel Souque <samuel.souque@outlook.fr>
2016-06-24 14:01:41 +0200

    "valid" column first implementation, minor changes in json

--- prototype1/script.js
+++ prototype1/script.js
@@ -26,39 +26,62 @@ var app = angular.module('goldenPasswordApp', [])
 
                     var min = new RegExp('[^]{' + passwordRules.minimalLength + ',}');
                     if (passwordRules.maximalLength != 0) {
-                        var max = new RegExp('^[^]{0,' + passwordRules.maximalLength + '}$');
+                        var max = new RegExp('^[^]{1,' + passwordRules.maximalLength + '}$');
                     }
                     var lcL = /[a-z]+/;
                     var upL = /[A-Z]+/;
                     var num = /[0-9]+/;
-                    var ascii = /[\x21-\xFF]+/;
+                    var ascii = /[\x21-\x2F\x3A-\x40\x5B-\x60\x7B-\xFF]+/;
                     var unicode = /[^\x00-\xFF]+/;
-                    if (passwordRules.allowedSymbols != "") {
-                        var allowed = new RegExp('[' + passwordRules.allowedSymbols + ']+');
+                    if (passwordRules.allowedSymbols != '') {
+                        var onlyAllowed = new RegExp('^[a-zA-Z0-9' + passwordRules.allowedSymbols + ']+$');
                     }
-                    if (passwordRules.forbiddenSymbols != "") {
-                        var restricted = new RegExp('[' + passwordRules.forbiddenSymbols + ']+');
+                    if (passwordRules.forbiddenSymbols != '') {
+                        var forbidden = new RegExp('[' + passwordRules.forbiddenSymbols + ']+');
                     }
 
+                    var respectMinL = min.test(scope.password);
+                    var respectMaxL = !max || max.test(scope.password);
                     var containsLcL = lcL.test(scope.password);
-                    var containsUpL = lcL.test(scope.password);
+                    var containsUpL = upL.test(scope.password);
                     var containsNum = num.test(scope.password);
                     var containsAscii = ascii.test(scope.password);
                     var containsUnicode = unicode.test(scope.password);
-                    var containsAllowed = allowed.test(scope.password);
-                    var containsRestricted = allowed.test(scope.password);
+                    var containsOnlyAllowed = !onlyAllowed || onlyAllowed.test(scope.password);
+                    var notContainsForbidden = !forbidden || !forbidden.test(scope.password);
 
-                    var count = 0;
-                    if (containsLcL) { count++; };
-                    if (containsUpL) { count++; };
-                    if (containsNum) { count++; };
-                    if (containsUnicode || containsAllowed) { count++; };
+                    if (passwordRules.complexity == 'basic') {
+                        var complexEnough = true;                        
+                    } else {
+                        var count = 0;
+                        if (containsLcL) { count++; };
+                        if (containsUpL) { count++; };
+                        if (containsNum) { count++; };
+                        if (containsAscii || containsUnicode) { count++; };
+                        
+                        if (passwordRules.complexity == '2class') {
+                            var complexEnough = count == 2;
+                        }
+                        if (passwordRules.complexity == '3class') {
+                            var complexEnough = count == 3;
+                        }
+                        if (passwordRules.complexity == '4class') {
+                            var complexEnough = count == 4;
+                        }
+                    }
 
-                    return min.test(scope.password)
-                        && (!max || max.test(scope.password))
-                        /*&& (!lcL || lcL.test(scope.password))
-                        && (!upL || upL.test(scope.password))
-                        && (!num || num.test(scope.password));*/
+                    return respectMinL
+                        && respectMaxL
+                        && (passwordRules.lowercaseLetters == 2 || passwordRules.lowercaseLetters == 3 && containsLcL)
+                        && (passwordRules.uppercaseLetters == 2 || passwordRules.uppercaseLetters == 3 && containsUpL)
+                        && (passwordRules.numerals == 2 || passwordRules.numerals == 3 && containsNum)
+                        && (passwordRules.nonAlphanumeric == 2
+                            || passwordRules.nonAlphanumeric == 0 && !containsAscii
+                            || passwordRules.nonAlphanumeric == 1 && containsOnlyAllowed && notContainsForbidden)
+                        && (passwordRules.unicodeCharacters == 2
+                            || passwordRules.unicodeCharacters == 0 && !containsUnicode
+                            || passwordRules.unicodeCharacters == 1 && containsOnlyAllowed && notContainsForbidden)
+                        && complexEnough;
                 };
 
                 scope.$watch('password', function (newValue, oldValue) {
