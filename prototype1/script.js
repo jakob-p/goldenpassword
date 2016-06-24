a6f97bbd77ceffd66dbf96f82d9e6d8c3f3e42fb
Samuel Souque <samuel.souque@outlook.fr>
2016-06-24 14:51:01 +0200

    "mandatory" column first implementation todo: refactorize code to share one function returning the state object

--- prototype1/script.js
+++ prototype1/script.js
@@ -60,13 +60,13 @@ var app = angular.module('goldenPasswordApp', [])
                         if (containsAscii || containsUnicode) { count++; };
                         
                         if (passwordRules.complexity == '2class') {
-                            var complexEnough = count == 2;
+                            var complexEnough = count > 1;
                         }
                         if (passwordRules.complexity == '3class') {
-                            var complexEnough = count == 3;
+                            var complexEnough = count > 2;
                         }
                         if (passwordRules.complexity == '4class') {
-                            var complexEnough = count == 4;
+                            var complexEnough = count > 3;
                         }
                     }
 
@@ -94,6 +94,66 @@ var app = angular.module('goldenPasswordApp', [])
     .directive('gpMandatoryCell', function () {
         return {
             restrict: 'E',
+            scope: {
+                rowObject: '=',
+                password: '='
+            },
+            link: function (scope, elmt, attrs) {
+                function mandatoryState(password) {
+                    var passwordRules = scope.rowObject.passwordRules;
+                    var state = {};
+
+                    var min = new RegExp('[^]{' + passwordRules.minimalLength + ',}');
+                    if (passwordRules.maximalLength != 0) {
+                        var max = new RegExp('^[^]{1,' + passwordRules.maximalLength + '}$');
+                    }
+                    var lcL = /[a-z]+/;
+                    var upL = /[A-Z]+/;
+                    var num = /[0-9]+/;
+                    var ascii = /[\x21-\x2F\x3A-\x40\x5B-\x60\x7B-\xFF]+/;
+                    var unicode = /[^\x00-\xFF]+/;
+                    if (passwordRules.allowedSymbols != '') {
+                        var onlyAllowed = new RegExp('^[a-zA-Z0-9' + passwordRules.allowedSymbols + ']+$');
+                    }
+                    if (passwordRules.forbiddenSymbols != '') {
+                        var forbidden = new RegExp('[' + passwordRules.forbiddenSymbols + ']+');
+                    }
+
+                    state.minL = min.test(scope.password);
+                    state.maxL = !max || max.test(scope.password);
+                    state.lcL = lcL.test(scope.password);
+                    state.upL = upL.test(scope.password);
+                    state.num = num.test(scope.password);
+                    state.ascii = ascii.test(scope.password);
+                    state.unicode = unicode.test(scope.password);
+
+                    if (passwordRules.complexity == 'basic') {
+                        state.complexEnough = true;                        
+                    } else {
+                        var count = 0;
+                        if (state.lcL) { count++; };
+                        if (state.upL) { count++; };
+                        if (state.num) { count++; };
+                        if (state.ascii || state.unicode) { count++; };
+
+                        if (passwordRules.complexity == '2class') {
+                            state.complexEnough = count > 1;
+                        }
+                        if (passwordRules.complexity == '3class') {
+                            state.complexEnough = count > 2;
+                        }
+                        if (passwordRules.complexity == '4class') {
+                            state.complexEnough = count > 3;
+                        }
+                    }
+
+                    return state;
+                };
+
+                scope.$watch('password', function (newValue, oldValue) {
+                    scope.state = mandatoryState();
+                });
+            },
             templateUrl: 'gp-mandatory-cell.html'
         };
     });
