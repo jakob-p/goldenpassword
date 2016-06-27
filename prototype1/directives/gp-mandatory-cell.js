76820bccd52ca68cf94922556268d95a0cb132a1
bAckmumu <bAckmumu@gmail.com>
2016-06-27 16:18:01 +0200

    fixed red max length if password is null

--- prototype1/directives/gp-mandatory-cell.js
+++ prototype1/directives/gp-mandatory-cell.js
@@ -11,8 +11,8 @@ app.directive('gpMandatoryCell', function () {
                 var state = {};
 
                 var min = new RegExp('[^]{' + passwordRules.minimalLength + ',}');
-                if (passwordRules.maximalLength != 0) {
-                    var max = new RegExp('^[^]{0,' + passwordRules.maximalLength + '}$');
+                if (passwordRules.maximalLength !== 0) {
+                    var max = new RegExp('^[^]{1,' + passwordRules.maximalLength + '}$');
                 }
                 var lcL = /[a-z]+/;
                 var upL = /[A-Z]+/;
@@ -27,7 +27,7 @@ app.directive('gpMandatoryCell', function () {
                 }
 
                 state.minL = min.test(scope.password);
-                state.maxL = !max || max.test(scope.password);
+                state.maxL = !max || scope.password.length == 0 || max.test(scope.password);
                 state.lcL = lcL.test(scope.password);
                 state.upL = upL.test(scope.password);
                 state.num = num.test(scope.password);
@@ -38,10 +38,10 @@ app.directive('gpMandatoryCell', function () {
                     state.complexEnough = true;
                 } else {
                     var count = 0;
-                    if (state.lcL) { count++; };
-                    if (state.upL) { count++; };
-                    if (state.num) { count++; };
-                    if (state.ascii || state.unicode) { count++; };
+                    if (state.lcL) { count++; }
+                    if (state.upL) { count++; }
+                    if (state.num) { count++; }
+                    if (state.ascii || state.unicode) { count++; }
 
                     if (passwordRules.complexity == '2class') {
                         state.complexEnough = count > 1;
