9855d96dcf3c54766f57321d98789f016d0822b2
jakob <jakob.pfab@gmail.com>
2016-07-25 01:43:21 +0200

    + move App to parent directory

---
+++ script.js
@@ -0,0 +1,15 @@
+﻿'use sctrict';
+
+var app = angular.module('goldenPasswordApp', ['ngclipboard','sticky'])
+    .controller('fetchGridData', ['$scope', '$http', '$rootScope', function ($scope, $http, $rootScope) {
+        $rootScope.showSuggestions = [];
+        $rootScope.suggestions = [];
+        $rootScope.randomWords = [];
+        $scope.password = '';
+        $scope.gridData = [];
+        $http.get('passwordData.json').then(function (response) {
+            $scope.gridData = response.data;
+        });
+
+
+    }]);
