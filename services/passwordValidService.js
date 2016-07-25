f7ae7abbc7655fdee4fda41b56cee65b9b923e48
jakob <jakob.pfab@gmail.com>
2016-07-25 18:27:58 +0200

    + suggestions fit more often (loop n=20) + delay when password input

--- services/passwordValidService.js
+++ services/passwordValidService.js
@@ -36,39 +36,54 @@ app.factory('passwordValidService', ['passwordRulesService', 'dictionaryService'
                         suggestion = '';
                         suggestion = suggestionService.addWord(suggestion);
                         suggestion = suggestionService.getSuggestions(suggestion, state, passwordRules);
+                        while (!isValid(passwordRulesService(suggestion, passwordRules), suggestion)) {
+                            suggestion = '';
+                            suggestion = suggestionService.addWord(suggestion);
+                            suggestion = suggestionService.getSuggestions(suggestion, state, passwordRules);
+                            counter++;
+                            if (counter > 20) {
+                                break;
+                            }
+                        }
+
                         if (isValid(passwordRulesService(suggestion, passwordRules), suggestion)) {
                             $rootScope.suggestions[index] = suggestion;
                             $rootScope.showSuggestions[index] = true;
-                        } else {
+                        }
+
+                        else {
                             $rootScope.showSuggestions[index] = false;
                         }
+
                     }
                 }
             }
         }
 
 
-        function isValid(state, password) {
-            var valid =
-                state.minL
-                && state.maxL
-                && (passwordRules.lowercaseLetters == 2 || passwordRules.lowercaseLetters == 3 && state.lcL)
-                && (passwordRules.uppercaseLetters == 2 || passwordRules.uppercaseLetters == 3 && state.upL)
-                && (passwordRules.numerals == 2 || passwordRules.numerals == 3 && state.num)
-                && (passwordRules.nonAlphanumeric == 2
-                || passwordRules.nonAlphanumeric == 0 && !state.ascii
-                || passwordRules.nonAlphanumeric == 1 && state.onlyAllowed && state.anyForbidden)
-                && (passwordRules.unicodeCharacters == 2
-                || passwordRules.unicodeCharacters == 0 && !state.unicode
-                || passwordRules.unicodeCharacters == 1 && state.onlyAllowed && state.anyForbidden)
-                && state.onlyAllowed
-                && state.anyForbidden
-                && state.complexEnough
-                && (passwordRules.dictionaryCheck == 'no'
-                || passwordRules.dictionaryCheck == 'yes' && !dictionaryService(password));
-            return valid;
-        }
 
-
+    function isValid(state, password) {
+        var valid =
+            state.minL
+            && state.maxL
+            && (passwordRules.lowercaseLetters == 2 || passwordRules.lowercaseLetters == 3 && state.lcL)
+            && (passwordRules.uppercaseLetters == 2 || passwordRules.uppercaseLetters == 3 && state.upL)
+            && (passwordRules.numerals == 2 || passwordRules.numerals == 3 && state.num)
+            && (passwordRules.nonAlphanumeric == 2
+            || passwordRules.nonAlphanumeric == 0 && !state.ascii
+            || passwordRules.nonAlphanumeric == 1 && state.onlyAllowed && state.anyForbidden)
+            && (passwordRules.unicodeCharacters == 2
+            || passwordRules.unicodeCharacters == 0 && !state.unicode
+            || passwordRules.unicodeCharacters == 1 && state.onlyAllowed && state.anyForbidden)
+            && state.onlyAllowed
+            && state.anyForbidden
+            && state.complexEnough
+            && (passwordRules.dictionaryCheck == 'no'
+            || passwordRules.dictionaryCheck == 'yes' && !dictionaryService(password));
+        return valid;
     }
-}]);
+
+
+}
+}])
+;
